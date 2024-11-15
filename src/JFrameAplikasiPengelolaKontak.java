295086598a0dbcd9dbead31c5fa04cb890765f5f
ahmadyasser72 <ahmadyasser72@smk.belajar.id>
2024-11-15 18:12:14 +0800

    ubah tema jadi windows classic

--- src/JFrameAplikasiPengelolaKontak.java
+++ src/JFrameAplikasiPengelolaKontak.java
@@ -523,7 +523,7 @@ public final class JFrameAplikasiPengelolaKontak extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Windows Classic".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
